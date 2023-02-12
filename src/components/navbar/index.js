9779fc8043f60058c295ed6b257e359955a47002
vinekalaylani <vinaekalaylani@gmail.com>
2023-02-12 14:27:57 +1100

    revision

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -2,7 +2,7 @@ export default function NavbarComp() {
     return (
         <nav className="navbar navbar-expand-lg bg-dark navbar-dark shadow-sm py-3 py-lg-0 px-3 px-lg-0">
             <a href="index.html" className="navbar-brand d-block d-lg-none">
-                <h1 className="m-0 text-uppercase text-white"><i className="fa fa-birthday-cake fs-1 text-primary me-3"></i>CakeZone</h1>
+                <h1 className="m-0 text-uppercase text-white"><i className="fa fa-birthday-cake fs-1 text-primary me-3"></i>DessertZone</h1>
             </a>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
                 <span className="navbar-toggler-icon"></span>
